773ac2acdc1b41dbd67494072b30f9db1d52dc22
argeento <adrian.wieprzkowicz@gmail.com>
2021-07-04 00:52:15 +0200

    feat: add stats to sidebar

---
+++ components/sidebar-stats.vue
@@ -0,0 +1,83 @@
+<template>
+  <div class="stats">
+    <div class="box box--questions">
+      <div class="number" v-text="questionsCount" />
+      zapytań
+    </div>
+    <div class="box box--answers">
+      <div class="number" v-text="answersCount" />
+      odpowiedzi
+    </div>
+    <div class="box box--comments">
+      <div class="number" v-text="commentsCount" />
+      komentarzy
+    </div>
+    <div class="box box--users">
+      <div class="number" v-text="usersCount" />
+      pasjonatów
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import { httpService } from '~/services/http.service'
+
+export default Vue.extend({
+  data() {
+    return {
+      questionsCount: 0,
+      answersCount: 0,
+      commentsCount: 0,
+      usersCount: 0,
+    }
+  },
+  async fetch() {
+    const stats = await httpService.statistics.getStatistics()
+    this.questionsCount = stats.data.questionsCount
+    this.answersCount = stats.data.answersCount
+    this.commentsCount = stats.data.commentsCount
+    this.usersCount = stats.data.usersCount
+  },
+})
+</script>
+
+<style lang="scss" scoped>
+.stats {
+  display: flex;
+  flex-wrap: wrap;
+  text-align: center;
+  margin-bottom: $gap-width;
+
+  @include mobile {
+    display: none;
+  }
+}
+
+.box {
+  width: 50%;
+  padding: size(15) 0;
+  color: var(--sidebar--stats--text-color);
+
+  &--questions {
+    background-color: var(--sidebar--stats--questions--background-color);
+  }
+
+  &--answers {
+    background-color: var(--sidebar--stats--answers--background-color);
+  }
+
+  &--comments {
+    background-color: var(--sidebar--stats--comments--background-color);
+  }
+
+  &--users {
+    background-color: var(--sidebar--stats--users--background-color);
+  }
+}
+
+.number {
+  font-size: size(24);
+  margin-bottom: size(5);
+}
+</style>
--- components/sidebar.vue
+++ components/sidebar.vue
@@ -1,3 +1,5 @@
 <template>
-  <aside>sidebar</aside>
+  <aside>
+    <SidebarStats />
+  </aside>
 </template>
