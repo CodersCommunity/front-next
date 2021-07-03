22c7badbe856d2a196fe1fe172632b3f22955318
argeento <adrian.wieprzkowicz@gmail.com>
2021-07-03 07:12:55 +0200

    feat: sidebar

---
+++ components/sidebar.vue
@@ -0,0 +1,3 @@
+<template>
+  <aside>sidebar</aside>
+</template>
--- layouts/sidebar.vue
+++ layouts/sidebar.vue
@@ -1,6 +1,41 @@
 <template>
   <div>
     <TopBar />
-    <Nuxt />
+    <div class="layout-container">
+      <main class="content">
+        <Nuxt />
+      </main>
+      <Sidebar class="sidebar" />
+    </div>
   </div>
 </template>
+
+<style lang="scss" scoped>
+.layout-container {
+  display: flex;
+  margin-top: $gap-width;
+
+  @include mobile {
+    flex-direction: column;
+  }
+}
+
+.content {
+  width: 75%;
+  margin-right: $gap-width;
+
+  @include mobile {
+    margin-right: 0;
+    margin-bottom: $gap-width;
+    width: 100%;
+  }
+}
+
+.sidebar {
+  width: 25%;
+
+  @include mobile {
+    width: 100%;
+  }
+}
+</style>
