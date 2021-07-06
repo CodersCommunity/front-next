842386bec96f7d58ad8255c5d4c805a6d31dc391
argeento <adrian.wieprzkowicz@gmail.com>
2021-07-06 04:15:46 +0200

    test: add categories-item spec

--- test/categories-item.spec.ts
+++ test/categories-item.spec.ts
@@ -1,23 +1,61 @@
-import { mount } from '@vue/test-utils'
-import { CategoryDto } from '~/services/__generated-api'
+import { mount, shallowMount } from '@vue/test-utils'
+import Categories from '@/components/categories.vue'
 import CategoriesItem from '@/components/categories-item.vue'
+import { CategoryDto } from '~/services/__generated-api'
 
 const exampleCategory: CategoryDto = {
   id: 1,
-  description: 'description',
-  path: 'path',
+  title: 'category title',
+  description: 'category-description',
+  path: 'category-path',
+  slug: 'category-slug',
   questionsCount: 12,
-  slug: 'slug',
   subcategories: [],
-  title: 'title',
+}
+
+const propsData = {
+  depth: 0,
+  category: exampleCategory,
 }
 
 describe('CategoriesItem', () => {
-  test.todo('is a list item')
-  test.todo('has heading with level based on depth')
-  test.todo('has title')
-  test.todo('has questions count')
-  test.todo('has description')
-  test.todo('has subcategories')
+  test('is a list item', () => {
+    const wrapper = shallowMount(CategoriesItem, { propsData })
+    expect(wrapper.element.tagName).toBe('LI')
+  })
+
+  test('has title', () => {
+    const wrapper = shallowMount(CategoriesItem, { propsData })
+    expect(wrapper.text()).toContain(exampleCategory.title)
+  })
+
+  test('has questions count', () => {
+    const wrapper = shallowMount(CategoriesItem, { propsData })
+    expect(wrapper.text()).toContain(`${exampleCategory.questionsCount}`)
+  })
+
+  test('has description', () => {
+    const wrapper = shallowMount(CategoriesItem, { propsData })
+    expect(wrapper.text()).toContain(exampleCategory.description)
+  })
+
+  test('has subcategories', () => {
+    const wrapper = mount(CategoriesItem, { propsData })
+    expect(wrapper.findComponent(Categories).exists()).toBe(true)
+  })
+
+  test('has heading with level based on depth', () => {
+    for (let i = 0; i < 5; i++) {
+      const wrapper = mount(CategoriesItem, {
+        propsData: {
+          depth: i,
+          category: exampleCategory,
+        },
+      })
+
+      expect(wrapper.find(`h${i + 2}`).exists()).toBe(true)
+    }
+  })
+
   test.todo('links to questions by category slug')
 })
