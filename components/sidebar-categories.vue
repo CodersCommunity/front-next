39e125c56606f274250d0b03ed59cb4c09846f44
argeento <adrian.wieprzkowicz@gmail.com>
2021-07-07 03:06:28 +0200

    feat: add categories to sidebar

--- components/sidebar-categories.vue
+++ components/sidebar-categories.vue
@@ -1,9 +1,39 @@
 <template>
   <div class="categories">
     <h2 class="title">Kategorie Pytań</h2>
-    <h3 class="title">Programowanie</h3>
+    <ul>
+      <li v-for="category in categories" :key="category.ui" class="item">
+        {{ category.title }}
+      </li>
+    </ul>
+
+    <div v-for="category in categoriesWithSubcategories" :key="category.id">
+      <h3 class="title">{{ category.title }}</h3>
+      <ul>
+        <li v-for="subcategory in category.subcategories" :key="subcategory.id">
+          {{ subcategory.title }}
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  computed: {
+    categories() {
+      return this.$accessor.categories
+    },
+    categoriesWithSubcategories() {
+      return this.$accessor.categories.filter(
+        (category) => category.subcategories.length > 0
+      )
+    },
+  },
+})
+</script>
 
 <style lang="scss" scoped>
 .categories {
@@ -17,7 +47,7 @@
   font-size: size(16);
   font-weight: normal;
 
-  padding-bottom: size(5);
+  padding: size(5);
   border-bottom: 1px solid var(--sidebar--categories--text-color);
 }
 </style>
