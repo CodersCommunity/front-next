911dd420d95b5bb8f5c2a93525f2e528d3754a0d
argeento <adrian.wieprzkowicz@gmail.com>
2021-07-14 13:41:23 +0200

    feat: button styles

--- components/button.vue
+++ components/button.vue
@@ -8,6 +8,7 @@
 import Vue from 'vue'
 
 enum Color {
+  Primary = 'primary',
   Info = 'info',
   Success = 'success',
   Warning = 'warning',
@@ -22,17 +23,22 @@ export default Vue.extend({
     },
     color: {
       type: String,
-      default: Color.Info,
+      default: Color.Primary,
       validator(value) {
-        return [Color.Info, Color.Success, Color.Warning, Color.Error].includes(
-          value as Color
-        )
+        return [
+          Color.Info,
+          Color.Success,
+          Color.Warning,
+          Color.Error,
+          Color.Primary,
+        ].includes(value as Color)
       },
     },
   },
   computed: {
     buttonCssClasses(): Object {
       return {
+        'button--primary': this.color === Color.Primary,
         'button--info': this.color === Color.Info,
         'button--success': this.color === Color.Success,
         'button--warning': this.color === Color.Warning,
@@ -46,5 +52,56 @@ export default Vue.extend({
 <style lang="scss" scoped>
 .button {
   cursor: pointer;
+  color: var(--button--text-color);
+  border: none;
+  padding: size(10) size(15);
+  font-size: size(14);
+
+  transition: background-color 0.15s;
+
+  &--primary {
+    background-color: var(--button--primary--background-color);
+
+    &:hover,
+    &:focus {
+      background-color: var(--button--primary--hover--background-color);
+    }
+  }
+
+  &--info {
+    background-color: var(--button--info--background-color);
+
+    &:hover,
+    &:focus {
+      background-color: var(--button--info--hover--background-color);
+    }
+  }
+
+  &--success {
+    background-color: var(--button--success--background-color);
+
+    &:hover,
+    &:focus {
+      background-color: var(--button--success--hover--background-color);
+    }
+  }
+
+  &--warning {
+    background-color: var(--button--warning--background-color);
+
+    &:hover,
+    &:focus {
+      background-color: var(--button--warning--hover--background-color);
+    }
+  }
+
+  &--error {
+    background-color: var(--button--error--background-color);
+
+    &:hover,
+    &:focus {
+      background-color: var(--button--error--hover--background-color);
+    }
+  }
 }
 </style>
