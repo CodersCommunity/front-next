dccb99b033ce49574c66e9d187f2c3083cb19f5d
Dominik Bendkowski <57157161+d-bend@users.noreply.github.com>
2021-07-20 20:53:33 +0200

    feat: styled sidebar-change-theme
    * styled sidebar-change-theme
    
    * formatting fix
    
    * changed according to feedback
    
    * changed according to feedback: round two

--- components/sidebar-change-theme.vue
+++ components/sidebar-change-theme.vue
@@ -1,7 +1,46 @@
 <template>
-  <div>
-    Motyw:
-    <button @click="$colorMode.preference = 'light'">Jasny</button>
-    <button @click="$colorMode.preference = 'dark'">Ciemny</button>
+  <div class="theme-wrapper">
+    <span class="label">Motyw:</span>
+    <div class="buttons-wrapper">
+      <button class="button" @click="$colorMode.preference = 'light'">
+        Jasny
+      </button>
+      <button class="button dark" @click="$colorMode.preference = 'dark'">
+        Ciemny
+      </button>
+    </div>
   </div>
 </template>
+
+<style lang="scss" scoped>
+.theme-wrapper {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+  background: var(--box--background-color);
+  padding: size(14);
+  border: 1px solid var(--box--border-color);
+}
+
+.label {
+  font-size: size(20);
+  margin: 4px;
+}
+
+.buttons-wrapper {
+  display: flex;
+}
+
+.button {
+  width: size(80);
+  height: size(40);
+  border-radius: 0;
+  border: none;
+}
+
+.dark {
+  color: var(--sidebar--stats--text-color);
+  background: var(--sidebar--categories--background-color);
+}
+</style>
