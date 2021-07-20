77fcdbb6800da1df5fbaa4345e8e126e6e03f5dc
Dominik <bendkowski.dominik@gmail.com>
2021-07-20 20:30:11 +0200

    changed according to feedback

--- components/sidebar-change-theme.vue
+++ components/sidebar-change-theme.vue
@@ -1,50 +1,46 @@
 <template>
-  <div class='change_theme__container'>
-    <span>Motyw:</span>
-    <div class='change_theme_button__container'>
-      <button class='change_theme_button' @click="$colorMode.preference = 'light'">Jasny</button>
-      <button class='change_theme_button' @click="$colorMode.preference = 'dark'">Ciemny</button>
-
+  <div class="wrapper">
+    <span class="label">Motyw:</span>
+    <div class="buttons-wrapper">
+      <button class="button" @click="$colorMode.preference = 'light'">
+        Jasny
+      </button>
+      <button class="button dark" @click="$colorMode.preference = 'dark'">
+        Ciemny
+      </button>
     </div>
   </div>
 </template>
 
-<style lang='scss' scoped>
-.change_theme {
-  &__container {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    width: 100%;
-    background: var(--box--background-color);
-    padding: .8em;
-    border: 1px solid var(--box--border-color);
-
-    span {
-      font-size: 1.2em;
-      margin: 4px;
-    }
-  }
-
-  &_button {
-    &__container {
-      display: flex;
-    }
-
-    &:last-child {
-      color: var(--sidebar--stats--text-color);
-      background: var(--sidebar--categories--background-color);
-    }
-
-    &:hover {
-      cursor: pointer;
-    }
-
-    width: 5em;
-    height: 2.5em;
-    border-radius: 0;
-    border: 0 none;
-  }
+<style lang="scss" scoped>
+.wrapper {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+  background: var(--box--background-color);
+  padding: size(12);
+  border: 1px solid var(--box--border-color);
 }
 
+.label {
+  font-size: size(20);
+  margin: 4px;
+}
+
+.buttons-wrapper {
+  display: flex;
+}
+
+.button {
+  width: size(80);
+  height: size(40);
+  border-radius: 0;
+  border: 0 none;
+}
+
+.dark {
+  color: var(--sidebar--stats--text-color);
+  background: var(--sidebar--categories--background-color);
+}
 </style>
