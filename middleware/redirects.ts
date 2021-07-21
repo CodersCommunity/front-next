38eb3d9d2f899ff8d32c258a3bd6b45fd5406d5a
Dominik <bendkowski.dominik@gmail.com>
2021-07-21 19:11:41 +0200

    Added start query param transformation, refactored

--- middleware/redirects.ts
+++ middleware/redirects.ts
@@ -12,17 +12,66 @@ export default function ({ route, redirect }: Context) {
   }
 
   if (route.path.match(/^\/questions/)) {
-    let params = route.fullPath.split('?')[1]
+    if (!route.query) return redirect(301, '/pytania')
 
-    if (!params) return redirect(301, '/pytania')
+    const newQueryParams = {}
 
-    for (const paramOrOption of Object.keys(questionsEngToPl)) {
-      params = params.replace(
-        paramOrOption,
-        // @ts-ignore
-        questionsEngToPl[paramOrOption]
-      )
-    }
-    redirect(301, `/pytania?${params}`)
+    translateKeysAndValuesToPL(route.query, newQueryParams, questionsEngToPl)
+
+    transformStartToPage(route.query, newQueryParams)
+
+    const newUrl = makeNewUrl('/pytania', newQueryParams)
+
+    redirect(301, newUrl)
   }
+}
+
+function transformStartToPage(
+  queryParams: any,
+  newQueryParams: Record<string, string>
+) {
+  const parsedStart = parseInt(queryParams.start)
+  if (!parsedStart) return
+
+  let calculatedPage = Math.floor(parseInt(queryParams.start) / 20) + 1
+
+  if (!calculatedPage) calculatedPage = 1
+
+  // @ts-ignore
+  newQueryParams.strona = calculatedPage
+}
+
+function translateKeysAndValuesToPL(
+  queryParams: any,
+  newQueryParams: Record<string, string>,
+  translationLookup: Record<string, string>
+) {
+  Object.keys(queryParams).reduce(
+    (total: Record<string, string>, current: string) => {
+      // @ts-ignore
+      const keyTranslation = translationLookup[current]
+      if (keyTranslation) {
+        // @ts-ignore
+        total[keyTranslation] = queryParams[current]
+      }
+
+      const valueTranslation = translationLookup[queryParams[current]]
+      if (valueTranslation) {
+        total[keyTranslation] = valueTranslation
+      }
+
+      return total
+    },
+    newQueryParams
+  )
+}
+
+function makeNewUrl(baseRoute: string, newQueryParams: Record<string, string>) {
+  baseRoute += '?'
+  for (const key of Object.keys(newQueryParams)) {
+    // @ts-ignore
+    const keyValue = `${key}=${newQueryParams[key]}`
+    baseRoute += `${keyValue}`
+  }
+  return baseRoute
 }
