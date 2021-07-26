15a519ee3a4457da71e996e7146449168c59f195
Dominik <bendkowski.dominik@gmail.com>
2021-07-26 16:18:11 +0200

    feat: InlineUser sets username color based on user level

--- components/inline-user.vue
+++ components/inline-user.vue
@@ -1,13 +1,35 @@
 <template>
   <span>
     <span v-if="user.favourite" class="favourite">★</span>
-    {{ user.name }} ({{ user.points }} p.)
+    <span :class="userLevel" data-test="username">{{ user.name }}</span>
+    <span class="points">({{ user.points }} p.)</span>
   </span>
 </template>
 
 <script lang="ts">
 import Vue, { PropType } from 'vue'
-import { InlineUserDto } from '~/services/__generated-api'
+import { InlineUserDto, UserLevelDto } from '~/services/__generated-api'
+
+function setUsernameStyle(level: UserLevelDto) {
+  switch (level) {
+    case 0:
+      return 'username'
+    case 10:
+      return 'username--approved'
+    case 20:
+      return 'username--expert'
+    case 50:
+      return 'username--editor'
+    case 80:
+      return 'username--moderator'
+    case 100:
+      return 'username--admin'
+    case 120:
+      return 'username--super-admin'
+    default:
+      return 'username'
+  }
+}
 
 export default Vue.extend({
   props: {
@@ -16,5 +38,41 @@ export default Vue.extend({
       required: true,
     },
   },
+  computed: {
+    userLevel() {
+      return setUsernameStyle(this.$props.user.level)
+    },
+  },
 })
 </script>
+
+<style lang="scss" scoped>
+.username {
+  font-weight: bold;
+  color: #898989;
+  &--approved {
+    font-weight: bold;
+    color: #000000ff;
+  }
+  &--expert {
+    font-weight: bold;
+    color: #6d0080;
+  }
+  &--editor {
+    font-weight: bold;
+    color: #007ca0;
+  }
+  &--moderator {
+    font-weight: bold;
+    color: #007200;
+  }
+  &--admin {
+    font-weight: bold;
+    color: #ec0000;
+  }
+  &--super-admin {
+    font-weight: bold;
+    color: #bc0000;
+  }
+}
+</style>
