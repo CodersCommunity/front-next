10108d3033e30306d0b935436f00cd6b084572b5
argeento <adrian.wieprzkowicz@gmail.com>
2021-07-27 01:51:41 +0200

    refactor: inline-user

--- components/inline-user.vue
+++ components/inline-user.vue
@@ -1,16 +1,14 @@
 <template>
   <span>
     <span v-if="user.favourite" class="favourite">★</span>
-    <span class="username--bold" :class="usernameStyle" data-test="username">{{
-      user.name
-    }}</span>
+    <span class="username" :class="usernameStyle">{{ user.name }}</span>
     <span class="points">({{ user.points }} p.)</span>
   </span>
 </template>
 
 <script lang="ts">
 import Vue, { PropType } from 'vue'
-import { InlineUserDto, UserLevelDto } from '~/services/__generated-api'
+import { InlineUserDto } from '~/services/__generated-api'
 
 export default Vue.extend({
   props: {
@@ -21,54 +19,53 @@ export default Vue.extend({
   },
   computed: {
     usernameStyle() {
-      return getUsernameStyle(this.$props.user.level)
+      switch (this.$props.user.level) {
+        case 0:
+          return ''
+        case 10:
+          return 'username--approved'
+        case 20:
+          return 'username--expert'
+        case 50:
+          return 'username--editor'
+        case 80:
+          return 'username--moderator'
+        case 100:
+          return 'username--admin'
+        case 120:
+          return 'username--super-admin'
+        default:
+          return ''
+      }
     },
   },
 })
-
-function getUsernameStyle(level: UserLevelDto) {
-  switch (level) {
-    case 0:
-      return 'username'
-    case 10:
-      return 'username--approved'
-    case 20:
-      return 'username--expert'
-    case 50:
-      return 'username--editor'
-    case 80:
-      return 'username--moderator'
-    case 100:
-      return 'username--admin'
-    case 120:
-      return 'username--super-admin'
-    default:
-      return 'username'
-  }
-}
 </script>
 
 <style lang="scss" scoped>
 .username {
-  &--bold {
-    font-weight: bold;
-  }
   color: var(--user--base--text-color);
+
   &--approved {
     color: var(--user--approved--text-color);
   }
+
   &--expert {
     color: var(--user--expert--text-color);
   }
+
   &--editor {
     color: var(--user--editor--text-color);
   }
+
   &--moderator {
     color: var(--user--moderator--text-color);
   }
+
   &--admin {
     color: var(--user--admin--text-color);
   }
+
   &--super-admin {
     color: var(--user--super-admin--text-color);
   }
