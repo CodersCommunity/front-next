75674abda7f8bbbf1e81d63bc973281f803bfac6
argeento <adrian.wieprzkowicz@gmail.com>
2021-08-03 12:15:46 +0200

    test: add inline category spec

---
+++ components/inline-category.vue.spec.ts
@@ -0,0 +1,44 @@
+import { createLocalVue, mount } from '@vue/test-utils'
+import VueRouter from 'vue-router'
+import InlineCategory from '@/components/inline-category.vue'
+import { CategoryDto } from '~/services/__generated-api'
+
+const category: CategoryDto = {
+  id: 1,
+  title: 'Category title',
+  description: 'category description',
+  path: 'category-path',
+  slug: 'category-slug',
+  questionsCount: 100,
+  subcategories: [],
+  position: 1,
+}
+
+describe('InlineCategory', () => {
+  it('links to category question list', () => {
+    const localVue = createLocalVue()
+    localVue.use(VueRouter)
+
+    const wrapper = mount(InlineCategory, {
+      propsData: { category },
+      router: new VueRouter({
+        mode: 'history',
+        routes: [{ name: 'pytania', path: '/pytania' }],
+      }),
+      localVue,
+    })
+
+    expect(wrapper.find('a').attributes('href')).toBe(
+      `/pytania?kategoria=${category.path}`
+    )
+  })
+
+  it('renders category name', () => {
+    const wrapper = mount(InlineCategory, {
+      stubs: ['router-link'],
+      propsData: { category },
+    })
+
+    expect(wrapper.text()).toContain(category.title)
+  })
+})
