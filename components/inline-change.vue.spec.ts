0544b10396d7ca5a5e2742cf5236ba4f092c3250
argeento <adrian.wieprzkowicz@gmail.com>
2021-08-03 12:48:23 +0200

    test: add inline change spec

---
+++ components/inline-change.vue.spec.ts
@@ -0,0 +1,64 @@
+import { shallowMount } from '@vue/test-utils'
+import InlineChange from '@/components/inline-change.vue'
+import InlineCategory from '@/components/inline-category.vue'
+import InlineUser from '@/components/inline-user.vue'
+import { CategoryDto, InlineQuestionDto } from '~/services/__generated-api'
+
+const change: InlineQuestionDto['change'] = {
+  date: new Date().toString(),
+  type: 'question_created',
+  showItemId: 12,
+  user: {
+    favourite: false,
+    id: 1,
+    level: 10,
+    name: 'username',
+    points: 100,
+    title: 'VIP',
+  },
+}
+
+const category: CategoryDto = {
+  id: 1,
+  title: 'Category title',
+  description: 'category description',
+  path: 'category-path',
+  slug: 'category-slug',
+  questionsCount: 100,
+  subcategories: [],
+  position: 1,
+}
+
+describe('inline-change', () => {
+  it('shows type of change', () => {
+    const wrapper = shallowMount(InlineChange, {
+      propsData: { change },
+    })
+
+    expect(wrapper.text()).toContain('pytanie zadane')
+  })
+
+  it('shows category if provided', () => {
+    const wrapper = shallowMount(InlineChange, {
+      propsData: { change, category },
+    })
+
+    expect(wrapper.findComponent(InlineCategory).exists()).toBeTruthy()
+  })
+
+  it('does not show category if not provided', () => {
+    const wrapper = shallowMount(InlineChange, {
+      propsData: { change },
+    })
+
+    expect(wrapper.findComponent(InlineCategory).exists()).toBeFalsy()
+  })
+
+  it('shows user', () => {
+    const wrapper = shallowMount(InlineChange, {
+      propsData: { change },
+    })
+
+    expect(wrapper.findComponent(InlineUser).exists()).toBeTruthy()
+  })
+})
